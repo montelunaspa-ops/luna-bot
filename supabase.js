98eb1bf1c67f3dd3e94e6c3e28bfb0cefc9643a2
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-23 12:58:58 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,4 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
