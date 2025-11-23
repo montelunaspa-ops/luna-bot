4fc028cbe31f98cd359d4198b3c00ebddbd1516e
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-23 12:24:09 -0300

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,15 @@
+import OpenAI from "openai";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+// Convierte nota de voz a texto
+export async function transcribirAudio(urlAudio) {
+  const response = await openai.audio.transcriptions.create({
+    file: await axios.get(urlAudio, { responseType: "arraybuffer" }),
+    model: "whisper-1"
+  });
+  return response.text;
+}
