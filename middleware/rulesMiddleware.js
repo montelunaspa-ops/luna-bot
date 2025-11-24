539cbb6eaa21fee7ea60fb4a74bc6c85a5376e1e
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-24 20:44:05 -0300

    Update rulesMiddleware.js

--- middleware/rulesMiddleware.js
+++ middleware/rulesMiddleware.js
@@ -1,1 +1,17 @@
+// /middleware/rulesMiddleware.js
+import { getRules } from "../utils/rulesLoader.js";
+
+export async function applyRulesToMessage(userMessage) {
+  const rules = await getRules();
+
+  return `
+Eres Luna, asistente virtual de Delicias Monte Luna.
+Debes seguir estrictamente las siguientes reglas actualizadas:
+
+${rules}
+
+Mensaje del cliente:
+${userMessage}
+`;
+}
 
