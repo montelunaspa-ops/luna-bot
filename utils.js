0f59785502bee26b6c972fb4aee473b368de99e0
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-24 13:25:48 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,19 +1,26 @@
-import fs from "fs";
-import fetch from "node-fetch";
 import OpenAI from "openai";
+import fetch from "node-fetch";
+import fs from "fs";
+import path from "path";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export async function transcribirAudio(url) {
-  const response = await fetch(url);
+// Función para transcribir audio
+export async function transcribirAudio(mediaUrl) {
+  const response = await fetch(mediaUrl);
   const buffer = Buffer.from(await response.arrayBuffer());
-  fs.writeFileSync("temp_audio.ogg", buffer);
+
+  const tmpPath = path.join("/tmp", "voz.mp3");
+  fs.writeFileSync(tmpPath, buffer);
+
   const transcription = await openai.audio.transcriptions.create({
-    file: fs.createReadStream("temp_audio.ogg"),
+    file: fs.createReadStream(tmpPath),
     model: "whisper-1"
   });
-  fs.unlinkSync("temp_audio.ogg");
+
+  fs.unlinkSync(tmpPath);
   return transcription.text;
 }
