3a3fe07d368c67858567b9120a73b0c6e2c878c6
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-27 21:00:30 -0300

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,9 +1,7 @@
-{
-  "queques_peruanos": {
-    "precio": 8500,
-    "porciones": ["14", "16", "sin cortar"],
-    "tamanio": "28 cm diámetro, 10 cm alto",
-    "sabores": [
+export default {
+  queques_peruanos: {
+    precio: 8500,
+    sabores: [
       "Chocolate",
       "Marmoleado",
       "Piña",
@@ -13,9 +11,9 @@
     ]
   },
 
-  "galletas_bandeja_20": {
-    "precio": 4000,
-    "sabores": [
+  bandejas: {
+    precio: 4000,
+    sabores: [
       "Rellena de Manjar",
       "Alemana",
       "Giro Coco",
@@ -27,30 +25,8 @@
     ]
   },
 
-  "muffins": {
-    "chips": { "precio": 3500, "cantidad": 6 },
-    "premium_surtido": {
-      "precio": 5000,
-      "cantidad": 6,
-      "detalles": [
-        "1 Chocolate",
-        "1 Red Velvet",
-        "1 Arándano",
-        "1 Coco",
-        "2 Chips"
-      ]
-    }
-  },
-
-  "delicias_premium": {
-    "alfajor_maicena": { "precio": 6000, "cantidad": 12 },
-    "cachitos_manjar": { "precio": 6000, "cantidad": 10 }
-  },
-
-  "queque_rectangular": {
-    "precio_unidad": 3000,
-    "oferta": "4 unidades por $10000",
-    "sabores": ["Vainilla Chips", "Manzana", "Arándanos"],
-    "tamano": "20 cm rectangular"
+  muffins: {
+    chips: 3500,
+    premium: 5000
   }
-}
+};
