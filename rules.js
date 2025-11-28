ccad92512568ac83c1f90e813a0bbfa7127fa611
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-27 21:00:16 -0300

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,48 @@
+export default {
+  intro: "Eres Luna 💛 asistente virtual. Responde corto y amable.",
+
+  mensaje_bienvenida:
+    "¡Hola! Soy Luna, asistente virtual de Delicias Monte Luna. 🌙✨\nAquí tienes nuestro catálogo oficial:",
+
+  comunas: [
+    "cerro navia",
+    "cerrillos",
+    "conchalí",
+    "estación central",
+    "independencia",
+    "lo prado",
+    "lo espejo",
+    "maipú",
+    "pedro aguirre cerda",
+    "pudahuel",
+    "quinta normal",
+    "recoleta",
+    "renca",
+    "santiago centro",
+    "san miguel",
+    "san joaquín"
+  ],
+
+  horarios: {
+    "cerro navia": "11-13",
+    "cerrillos": "11-13",
+    "conchalí": "12-14",
+    "estación central": "9-11",
+    "independencia": "11-14",
+    "lo prado": "11-13",
+    "lo espejo": "10-12",
+    "maipú": "10-12",
+    "pedro aguirre cerda": "10-12",
+    "pudahuel": "12-14",
+    "quinta normal": "10-13",
+    "recoleta": "11-13",
+    "renca": "10-13",
+    "santiago centro": "9-11",
+    "san miguel": "10-12",
+    "san joaquín": "10-12"
+  },
+
+  despacho_gratis: 14990,
+  costo_envio: 2400,
+  retiro_domicilio: "Calle Chacabuco 1120, Santiago Centro"
+};
