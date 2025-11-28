33762a7f7646bb786f91ea9c3bd93c7c44cd9536
montelunaspa-ops <montelunaspa@gmail.com>
2025-11-28 14:04:52 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,17 +1,9 @@
-// supabase.js — versión FINAL usando SUPABASE_KEY
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const url = process.env.SUPABASE_URL;
-const key = process.env.SUPABASE_KEY; // <── ESTA ES LA QUE TIENES EN RENDER
-
-if (!url) {
-  console.error("❌ ERROR: Falta SUPABASE_URL en Render.");
-}
-
-if (!key) {
-  console.error("❌ ERROR: Falta SUPABASE_KEY en Render.");
-}
-
-export const supabase = createClient(url, key);
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
