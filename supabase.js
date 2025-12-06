ad642c9e81da568e3d2fcc5c18fd8a73875905aa
montelunaspa-ops <montelunaspa@gmail.com>
2025-12-05 22:40:10 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-
-if (!SUPABASE_URL || !SUPABASE_KEY) {
-  console.log("⚠️ SUPABASE_URL o SUPABASE_KEY no están definidas en .env");
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  console.warn("⚠️ SUPABASE_URL o SUPABASE_KEY no están definidas");
 }
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
-module.exports = supabase;
+module.exports = { supabase };
